3d098536015a02272a401cbfb05eca30aa9287f3
CEED <fatbeebhw@proton.me>
2023-08-01 23:56:38 +0200

    Update to 3.0
    A very dirty update.
    
    - Very faster, 1000 Accounts in 10 seconds or under (depends on proxy tho)
    - Checks for Consent Locked
    - Checks for Unlockable Accounts
    - Updated UA.
    - Reworked some stuff around
    - Still the very same dirty code that just works.
    
    ** The tests were done only with token format, please run some tests and let me know on telegram @fatbeebhw**

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-VERSION = "2.8.1"
+VERSION = "3.0.0"
 
 PROFILE_UPDATE = "https://twitter.com/i/api/1.1/account/update_profile.json"
 
@@ -12,31 +12,31 @@ ACTIVATE_HEADERS = headers = {
     'origin': 'https://twitter.com',
     'pragma': 'no-cache',
     'referer': 'https://twitter.com/',
-    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
     'x-twitter-active-user': 'yes',
     'x-twitter-client-language': 'en',
 }
 
 STANDARD_HEADERS = {
-    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
     'DNT': '1',
     'x-twitter-client-language': 'en',
     'sec-ch-ua-mobile': '?0',
     'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
     'Referer': 'https://twitter.com/',
     'x-twitter-auth-type': 'OAuth2Session',
     'x-twitter-active-user': 'yes',
     'sec-ch-ua-platform': '"Windows"',
 }
 
-CHECK_HEADERS =  {
+CHECK_HEADERS = {
     'authority': 'api.twitter.com',
     'accept': '*/*',
     'accept-language': 'en-US,en;q=0.9',
@@ -46,13 +46,13 @@ CHECK_HEADERS =  {
     'origin': 'https://twitter.com',
     'pragma': 'no-cache',
     'referer': 'https://twitter.com/',
-    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
     'x-twitter-active-user': 'yes',
     'x-twitter-client-language': 'en',
 }
